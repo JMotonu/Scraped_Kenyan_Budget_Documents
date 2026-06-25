e9f11d752b0b70eae84c4abbac3c83062cbb9ff5
JMotonu <mtnjosephat@gmail.com>
2026-06-25 22:43:41 +0300

    add features: save; read; delete; update

---
+++ database/mongodb.py
@@ -0,0 +1,49 @@
+from pymongo import MongoClient
+from config import *
+
+client = MongoClient(MONGO_URI)
+
+db = client[DATABASE_NAME]
+
+collection = db[COLLECTION_NAME]
+
+
+def save_document(document):
+
+    existing = collection.find_one(
+        {"url": document["url"]}
+    )
+
+    if not existing:
+        collection.insert_one(document)
+
+
+def get_documents():
+
+    return list(collection.find())
+
+
+def get_document_by_title(title):
+
+    return collection.find_one(
+        {"title": title}
+    )
+
+
+def delete_document(doc_id):
+
+    from bson import ObjectId
+
+    collection.delete_one(
+        {"_id": ObjectId(doc_id)}
+    )
+
+
+def mark_viewed(doc_id):
+
+    from bson import ObjectId
+
+    collection.update_one(
+        {"_id": ObjectId(doc_id)},
+        {"$set": {"viewed": True}}
+    )
