707329d8cc9315f4c2e08a65cb827dd3467ccb5b
JMotonu <mtnjosephat@gmail.com>
2026-06-25 22:39:55 +0300

    store project settings in one place

---
+++ config.py
@@ -0,0 +1,9 @@
+PARLIAMENT_URL = "https://www.parliament.go.ke/budget-books-fy-2026-2027"
+
+TREASURY_URL = "https://www.treasury.go.ke/budget-books"
+
+MONGO_URI = "mongodb://localhost:27017/"
+
+DATABASE_NAME = "document_system"
+
+COLLECTION_NAME = "documents"
