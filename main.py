d0fa825ad3ea6a4d09536f845da87144026bebdb
JMotonu <mtnjosephat@gmail.com>
2026-06-25 23:06:08 +0300

    test

---
+++ main.py
@@ -0,0 +1,9 @@
+from scraper.parliament_scraper import scrape_parliament
+
+from scraper.treasury_scraper import scrape_treasury
+
+scrape_parliament()
+
+scrape_treasury()
+
+print("Scraping Complete")
