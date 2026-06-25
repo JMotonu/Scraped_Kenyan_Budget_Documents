962096832cc45be72bda2b1b432877d4e47fa5cf
JMotonu <mtnjosephat@gmail.com>
2026-06-25 23:51:53 +0300

    configure mongoDB atlas connection

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ PARLIAMENT_URL = "https://www.parliament.go.ke/budget-books-fy-2026-2027"
 
 TREASURY_URL = "https://www.treasury.go.ke/budget-books"
 
-MONGO_URI = "mongodb://localhost:27017/"
+MONGO_URI = "mongodb+srv://mtnjosephat_db_user:pOagX6p7ypaWh4yY@cluster0.dfpe6fj.mongodb.net/?appName=Cluster0"
 
 DATABASE_NAME = "document_system"
 
